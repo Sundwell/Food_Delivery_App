67b2e0229ce9bb40d6aaeb86e06c280115ca03be
serhii <lyamtsev.s2017@gmail.com>
2019-11-07 12:53:09 +0200

    Short: added ProductMainPageView.
    Added main page(popular) and all page(all products) with optional sorting

--- product/urls.py
+++ product/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import ProductMainPageView
+from .views import ProductMainPageView, ProductAllView
 
 app_name = 'product'
 
 urlpatterns = (
-    path('shop/main', ProductMainPageView.as_view(), name='main'),
+    path('main/', ProductMainPageView.as_view(), name='main'),
+    path('products/<str:slug>/', ProductAllView.as_view(), name='all'),
+    # path('products/<str:slug>/', ProductAllView.as_view(), name='sort_by'),
 )
